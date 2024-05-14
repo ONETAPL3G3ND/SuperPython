373a21642f91ca442ed70bc7c0684e711550c301
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-14 22:28:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+class Test1:
+    def __init__(self, name):
+        self.name = name
+
+class Test2(Test1):
+    def __init__(self, version, name):
+        self.version = version
+        super().__init__(name)
+
+class Test3(Test2):
+    def __init__(self, time, version, name):
+        self.time = time
+        super().__init__(version, name)
+
+if __name__ == "__main__":
+    t = Test3("12:00", "1.0", "Vasya")
+    print(vars(t))
